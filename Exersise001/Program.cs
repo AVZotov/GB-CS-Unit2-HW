f91596b1f3bf3058d3b97a46f60f78f905927f05
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-12 11:26:30 +0300

    Exersise001 project added

---
+++ Exersise001/Program.cs
@@ -0,0 +1,6 @@
+﻿int randomNumber = new Random().Next(100,1000);
+
+Console.WriteLine("Program to show second number from 3 digits number");
+Console.WriteLine($"Initial number: {randomNumber}");
+int secondDigit = (randomNumber / 10) % 10;
+Console.WriteLine($"Second digit: {secondDigit}");
