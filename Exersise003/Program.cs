b699949dce7993caed7234387a638789e4853a02
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-12 11:46:15 +0300

    Exersise003 project added

---
+++ Exersise003/Program.cs
@@ -0,0 +1,10 @@
+﻿int dayOfWeek = new Random().Next(1, 8);
+
+if (dayOfWeek == 6 || dayOfWeek == 7)
+{
+    Console.WriteLine($"day number {dayOfWeek} is a vacation day");
+}
+else
+{
+    Console.WriteLine($"day number {dayOfWeek} is working day");
+}
