bc5e9819b30b8a32bdcec06b7a6dd34929007b8c
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-12 11:41:54 +0300

    Exersise002 project added

---
+++ Exersise002/Program.cs
@@ -0,0 +1,12 @@
+﻿int randomNumber = new Random().Next(10, 1000);
+
+Console.WriteLine($"Initial number is: {randomNumber}");
+
+if (randomNumber / 100 > 0)
+{
+    Console.WriteLine($"third digit in number is: {randomNumber % 10}");
+}
+else
+{
+    Console.WriteLine("Number contains less then 3 digits");
+}
